f1756a87e10bc1fd19660ac02d49c4e398008115
Kranti Bijay Mohanty <kranti@kencloud.co.in>
2017-03-22 19:04:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kamran/login/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.kamran.login;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class MainActivity extends ActionBarActivity
+{
+    // Declaring layout button, edit texts
+    Button login;
+    EditText username,password;
+    ProgressBar progressBar;
+    // End Declaring layout button, edit texts
+
+    // Declaring connection variables
+    Connection con;
+    String un,pass,db,ip,in;
+    //End Declaring connection variables
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Getting values from button, texts and progress bar
+        login = (Button) findViewById(R.id.button);
+        username = (EditText) findViewById(R.id.editText);
+        password = (EditText) findViewById(R.id.editText2);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+        // End Getting values from button, texts and progress bar
+
+
+//        Online
+//        ip = "swash.database.windows.net";
+//        db = "KenCentral";
+//        un = "swashDB.Admin";
+//        pass = "Swash@2016";
+
+// Local
+//        ip = "SWASHDBS\\SWASHSQLINT";
+//        ip = "192.168.1.4";
+        // Port = 50857
+
+
+
+        ip = "192.168.1.4";
+        db = "KenCentral";
+        un = "induction";
+        pass = "swash@2015";
+        in = " SWASHDBS\\SWASHSQLINT";
+        // Name of the INSTANCE
+
+        // Declaring Server ip, username, database name and password
+
+
+        // Setting up the function when button login is clicked
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                CheckLogin checkLogin = new CheckLogin();// this is the Asynctask, which is used to process in background to reduce load on app process
+                checkLogin.execute("");
+            }
+        });
+        //End Setting up the function when button login is clicked
+    }
+
+    public class CheckLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
+
+        @Override
+        protected void onPreExecute()
+        {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String r)
+        {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, r, Toast.LENGTH_SHORT).show();
+            if(isSuccess)
+            {
+                Toast.makeText(MainActivity.this , "Login Successfull" , Toast.LENGTH_LONG).show();
+                //finish();
+            }
+        }
+        @Override
+        protected String doInBackground(String... params)
+        {
+            String usernam = username.getText().toString();
+            String passwordd = password.getText().toString();
+            if(usernam.trim().equals("")|| passwordd.trim().equals(""))
+                z = "Please enter Username and Password";
+            else
+            {
+                try
+                {
+                    con = connectionclass(un, pass, db, ip);        // Connect to database
+                    if (con == null)
+                    {
+                        z = "Check Your Internet Access!";
+                    }
+                    else
+                    {
+//                        String query = "select * from login where user_name= '" + usernam.toString() + "' and pass_word = '"+ passwordd.toString() +"'  ";
+                        String query = "select * from login where Master_Login_User= '" + usernam.toString() + "' and pass_word = '"+ passwordd.toString() +"'  ";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+                        if(rs.next())
+                        {
+                            z = "Login successful";
+                            isSuccess=true;
+                            con.close();
+                        }
+                        else
+                        {
+                            z = "Invalid Credentials!";
+                            isSuccess = false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isSuccess = false;
+                    z = ex.getMessage();
+                }
+            }
+            return z;
+        }
+    }
+
+
+    @SuppressLint("NewApi")
+    public Connection connectionclass(String user, String password, String database, String server)
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+//            ConnectionURL = "jdbc:jtds:sqlserver://" + server + database + ";user=" + user+ ";password=" + password + ";";
+            ConnectionURL = "jdbc:jtds:sqlserver://" + ip + db + ";user=" + un + ";password=" + pass + ";";
+//22 march
+//            ConnectionURL = "jdbc:jtds:sqlserver://" + ip + "/" + db + ";instance=" + in +";user=" + un + ";password=" + pass + ";";
+
+//            ConnectionURL = "jdbc:jtds:sqlserver://192.168.1.4: 50857" + "/" + db + ";instance=" + in +";user=" + un + ";password=" + pass + ";";
+
+
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
+    }
+}
